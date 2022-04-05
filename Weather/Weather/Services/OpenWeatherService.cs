330d462a34e847c1eb28e84b540ed1b13449c3b1
Carl-Henrik Lundin <lundin.carlhenrik@lundin.com>
2022-04-05 15:12:29 +0200

    Weather_Services

--- Weather/Weather/Services/OpenWeatherService.cs
+++ Weather/Weather/Services/OpenWeatherService.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Json; //Requires nuget package System.Net.Http.Json
+using System.Net.Http.Json;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
@@ -12,9 +12,84 @@ using Weather.Models;
 
 namespace Weather.Services
 {
-    //You replace this class witth your own Service from Project Part A
     public class OpenWeatherService
     {
+        HttpClient httpClient = new HttpClient();
+        readonly string apiKey = "b7218ab5e346e4579e66e7686f6814dc"; //b7218ab5e346e4579e66e7686f6814dc
+
+        ConcurrentDictionary<string, (DateTime, Forecast)> data = new ConcurrentDictionary<string, (DateTime, Forecast)>();
+        public event EventHandler<string> WeatherForecastAvailable;
+
+        public async Task<Forecast> GetForecastAsync(string City)
+        {
+            if (data.TryGetValue(City, out var _cacheCity))
+            {
+                if ((int)(DateTime.Now - _cacheCity.Item1).TotalSeconds <= 60)
+                {
+                    WeatherForecastAvailable.Invoke(this, $"Cached weather forecast for {City} available.");
+                    return _cacheCity.Item2;
+                }
+            }
+
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+
+            data.TryAdd(City, (DateTime.Now, forecast));
+
+            WeatherForecastAvailable.Invoke(this, $"New weather forecast for {City} available.");
+            return forecast;
+
+        }
+        
+        
+        public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
+        {
+            if (data.TryGetValue($"{latitude},{longitude}", out var _cacheCity))
+            {
+                if ((int)(DateTime.Now - _cacheCity.Item1).TotalSeconds <= 60)
+                {
+                    WeatherForecastAvailable.Invoke(this, $"Cached weather forecast for ({latitude}, {longitude}) available.");
+                    return _cacheCity.Item2;
+                }
+            }
+
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+
+            data.TryAdd($"{latitude},{longitude}", (DateTime.Now, forecast));
+            
+            WeatherForecastAvailable.Invoke(this, $"New weather forecast for ({latitude}, {longitude}) available.");
+            return forecast;
+        }
+
+
+
+        private async Task<Forecast> ReadWebApiAsync(string uri)
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            WeatherApiData wd = await response.Content.ReadFromJsonAsync<WeatherApiData>();
+
+            List<ForecastItem> forecastItems = wd.list.Select(x => new ForecastItem
+            {
+                Temperature = x.main.temp,
+                DateTime = UnixTimeStampToDateTime(x.dt),
+                WindSpeed = x.wind.speed,
+                Description = x.weather[0].description,
+                Icon = x.weather[0].icon
+            }).ToList();
+
+            Forecast forecast = new Forecast();
+            forecast.City = wd.city.name;
+            forecast.Items = forecastItems;
+            
+            return forecast;
+        }
+
 
         private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
