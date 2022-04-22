7de563365bdf7f66ec2b0462ac5bc28e04ebed74
Carl-Henrik Lundin <lundin.carlhenrik@lundin.com>
2022-04-22 13:11:53 +0200

    Update

--- Weather/Weather/Views/ForecastPage.xaml.cs
+++ Weather/Weather/Views/ForecastPage.xaml.cs
@@ -43,37 +43,30 @@ namespace Weather.Views
             //This is making the first load of data
             MainThread.BeginInvokeOnMainThread(async () => {await LoadForecast();});
 
-
-            //CityName.Text = groupedforecast.City.ToString();
-
-            
         }
 
 
-        private async void ListViewItemTapped(object sender, ItemTappedEventArgs e)
-        {
-          //  var item = e.Item as Forecast;
-          //  if (item != null) await DisplayAlert("Item tapped", $"Rectangle selected: {item}", "OK");
-
-           // ((ListView)sender).SelectedItem = null;
-        }
 
         
         private async Task LoadForecast()
         {
-            //Heare you load the forecast 
-            loadedForecast = await service.GetForecastAsync(this.Title.ToString());
+            //Load forecast. 
+            loadedForecast = await service.GetForecastAsync(Title.ToString());
             groupedforecast.City = loadedForecast.City;
             groupedforecast.Items = loadedForecast.Items.GroupBy(item => item.DateTime.Date);
-            CityName.Text = groupedforecast.City.ToString();
-            CustomGroupedList.ItemsSource = groupedforecast.Items;
+            
+
+
+            //Update labels in xalm.
+            CityName.Text = $"Weather forecast for {groupedforecast.City}";
+            WeatherForecastList.ItemsSource = groupedforecast.Items;
             List<ForecastForOneDay> daySumForecastList = new List<ForecastForOneDay>();
-            groupedforecast.Items.Take(3).ToList().ForEach(x =>
+            groupedforecast.Items.Skip(1).Take(3).ToList().ForEach(x =>
             {
-                daySumForecastList.Add(new ForecastForOneDay { Day = $"{x.Key.DayOfWeek}", Date = $"{x.Key:M}", MaxTemperature = $"{(int)x.Max(m => m.Temperature)}°C", MinTemperature = $"{(int)x.Min(m => m.Temperature)}°C", WindSpeed = $"{x.Max(m => m.WindSpeed)} m/s", Icon = x.SingleOrDefault(s => s.DateTime.Hour == 20).Icon.ToString()});
+                daySumForecastList.Add(new ForecastForOneDay { Day = $"{x.Key.DayOfWeek}", Date = $"{x.Key:M}", MaxTemperature = $"{(int)x.Max(m => m.Temperature)}°C", MinTemperature = $"{(int)x.Min(m => m.Temperature)}°C", WindSpeed = $"{x.Max(m => m.WindSpeed)} m/s", Icon = x.SingleOrDefault(s => s.DateTime.Hour == 14).Icon.ToString()});
             });
-            CustomList.ItemsSource = daySumForecastList;
-           
+            ThreeDayWeatherList.ItemsSource = daySumForecastList;
+            CurrentWeatherUpdate(0);
         }
 
         
@@ -81,12 +74,31 @@ namespace Weather.Views
         private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             var pic = sender as Slider;
-            if (pic != null) 
-            wind.Text = $"Wind speed: {loadedForecast.Items[(int)pic.Value].WindSpeed}";
-            Temp.Text = $"Temperature: {loadedForecast.Items[(int)pic.Value].Temperature}";
-            description.Text = $"{loadedForecast.Items[(int)pic.Value].Description}";
-            CurrentWeather.Source = $"{loadedForecast.Items[(int)pic.Value].Icon}";
-            TimeNow.Text = $"The weather {loadedForecast.Items[(int)pic.Value].DateTime:f}";
+            if (pic != null)
+                CurrentWeatherUpdate((int)pic.Value);
+        }
+
+        private void CurrentWeatherUpdate(int index)
+        {
+            wind.Text = $"Wind speed: {loadedForecast.Items[index].WindSpeed} m/s";
+            Temp.Text = $"Temperature: {(int)loadedForecast.Items[index].Temperature}°C";
+            description.Text = $"{loadedForecast.Items[index].Description}";
+            currentWeather.Source = $"{loadedForecast.Items[index].Icon}";
+            TimeNow.Text = $"The weather {loadedForecast.Items[index].DateTime:f}";
+        }
+
+        private void ThreeDayWeatherList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            int thisDay = groupedforecast.Items.First().Count();
+
+            if (e.SelectedItemIndex == 0) { CurrentWeatherUpdate(thisDay + 4); slider.Value = thisDay + 4; }
+            if (e.SelectedItemIndex == 1) { CurrentWeatherUpdate(thisDay + 12); slider.Value = thisDay + 12; }
+            if (e.SelectedItemIndex == 2) { CurrentWeatherUpdate(thisDay + 20); slider.Value = thisDay + 20; }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+           await LoadForecast();
         }
     }
 }
